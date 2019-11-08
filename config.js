28745427bb24543cf165b798958b9c61326e4fda
tvanrys <tvanrys@fanshaweonline.ca>
2019-11-07 20:24:00 -0500

    fixed

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
     host: "localhost",
     port: 3306, // 8889 for mac
     uname: "root",
-    pword: "hopalong", // leave blank for windows
+    pword: "root", // leave blank for windows
     database: "db_favthings"
 }
 
